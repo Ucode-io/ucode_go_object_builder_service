57190b9cd836ded836cbbe009db04e541c515bc8
b3atez <azamatn676@gmail.com>
2024-04-08 09:36:45 +0500

    add table repo

--- storage/storage.go
+++ storage/storage.go
@@ -41,7 +41,18 @@ type FunctionRepoI interface {
 	Delete(ctx context.Context, req *nb.FunctionPrimaryKey) error
 }
 
-type TableRepoI interface{}
+type TableRepoI interface {
+	Create(ctx context.Context, req *nb.CreateTableRequest) (resp *nb.CreateTableResponse, err error)
+	GetByID(ctx context.Context, req *nb.TablePrimaryKey) (resp *nb.Table, err error)
+	GetAll(ctx context.Context, req *nb.GetAllTablesRequest) (resp *nb.GetAllTablesResponse, err error)
+	Update(ctx context.Context, req *nb.UpdateTableRequest) (resp *nb.Table, err error)
+	Delete(ctx context.Context, req *nb.TablePrimaryKey) error
+
+	GetListTableHistory(ctx context.Context, req *nb.GetTableHistoryRequest) (resp *nb.GetTableHistoryResponse, err error)
+	GetTableHistoryById(ctx context.Context, req *nb.TableHistoryPrimaryKey) (resp *nb.Table, err error)
+	RevertTableHistory(ctx context.Context, req *nb.RevertTableHistoryRequest) (resp *nb.TableHistory, err error)
+	InsertVersionsToCommit(ctx context.Context, req *nb.InsertVersionsToCommitRequest) (resp *nb.TableHistory, err error)
+}
 
 type FileRepoI interface {
 	Create(ctx context.Context, req *nb.CreateFileRequest) (resp *nb.File, err error)
