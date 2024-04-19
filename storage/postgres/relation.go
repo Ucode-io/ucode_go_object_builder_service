70800437b0b1de3ea8cb6902ca72fc13a6e9f5b6
o.muzropov <otabek.muzropov2001@gmail.com>
2024-04-19 17:49:31 +0500

    relation storage is implemented

--- storage/postgres/relation.go
+++ storage/postgres/relation.go
@@ -3,6 +3,7 @@ package postgres
 import (
 	"context"
 
+	"ucode/ucode_go_object_builder_service/config"
 	nb "ucode/ucode_go_object_builder_service/genproto/new_object_builder_service"
 	"ucode/ucode_go_object_builder_service/storage"
 
@@ -20,6 +21,26 @@ func NewRelationRepo(db *pgxpool.Pool) storage.RelationRepoI {
 }
 
 func (r *relationRepo) Create(ctx context.Context, req *nb.CreateRelationRequest) (resp *nb.CreateRelationRequest, err error) {
+	// conn := psqlpool.Get(req.ProjectId)
+	// defer conn.Close()
+
+	pool, err := pgxpool.ParseConfig("postgres://udevs123_b52a2924bcbe4ab1b6b89f748a2fc500_p_postgres_svcs:oka@65.109.239.69:5432/udevs123_b52a2924bcbe4ab1b6b89f748a2fc500_p_postgres_svcs?sslmode=disable")
+	if err != nil {
+		return nil, err
+	}
+
+	conn, err := pgxpool.NewWithConfig(ctx, pool)
+	if err != nil {
+		return nil, err
+	}
+
+	switch req.Type {
+	case config.MANY2DYNAMIC:
+	case config.MANY2MANY:
+	case config.MANY2ONE:
+	case config.ONE2ONE:
+	}
+
 	return resp, nil
 }
 
