59ab8ae6b5c92f9b11ce6624763650cb2076dc4c
Shokhrukh <shokhrukhsafarov2003@gmail.com>
2024-04-23 15:06:38 +0500

    static parent id added

--- storage/postgres/menu.go
+++ storage/postgres/menu.go
@@ -439,6 +439,8 @@ func (m *menuRepo) GetAll(ctx context.Context, req *nb.GetAllMenusRequest) (resp
 	whereStr := ""
 	if req.ParentId != "" {
 		whereStr += fmt.Sprintf(`m.parent_id = '%v' `, req.ParentId)
+	} else if req.ParentId == "" {
+		whereStr += fmt.Sprintf(`m.parent_id = '%v' `, "c57eedc3-a954-4262-a0af-376c65b5a284")
 	}
 	if req.TableId != "" {
 		if len(whereStr) != 0 {
