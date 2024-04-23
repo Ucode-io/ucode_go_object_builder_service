17fe3eaf99518d91e8720fd9ba8c9189a36941b7
Shokhrukh <shokhrukhsafarov2003@gmail.com>
2024-04-23 10:23:36 +0500

    get all in object builder

---
+++ storage/postgres/relation_test.go
@@ -0,0 +1,51 @@
+package postgres_test
+
+import (
+	"context"
+	"testing"
+	"ucode/ucode_go_object_builder_service/config"
+	nb "ucode/ucode_go_object_builder_service/genproto/new_object_builder_service"
+
+	"github.com/stretchr/testify/assert"
+	"google.golang.org/protobuf/types/known/structpb"
+)
+
+func createRelation(t *testing.T) string {
+	relation, err := strg.Relation().Create(context.Background(), &nb.CreateRelationRequest{
+		Id:                     CreateRandomId(t),
+		Type:                   config.MANY2ONE,
+		TableTo:                "nannie",
+		TableFrom:              "clement_rodriguez",
+		RelationFieldId:        CreateRandomId(t),
+		RelationToFieldId:      CreateRandomId(t),
+		ViewFields:             []string{},
+		RelationFieldSlug:      "",
+		DynamicTables:          []*nb.DynamicTable{},
+		Editable:               false,
+		IsUserIdDefault:        false,
+		Cascadings:             []*nb.Cascading{},
+		ObjectIdFromJwt:        false,
+		CascadingTreeTableSlug: "",
+		CascadingTreeFieldSlug: "",
+		Attributes:             &structpb.Struct{},
+		GroupFields:            []string{},
+		QuickFilters:           []*nb.QuickFilter{},
+		Columns:                []string{},
+		MultipleInsert:         false,
+		IsEditable:             false,
+		MultipleInsertField:    "",
+		UpdatedFields:          []string{},
+		DefaultLimit:           "",
+		DefaultValues:          []string{},
+		DefaultEditable:        false,
+	})
+
+	assert.NoError(t, err)
+	assert.NotEmpty(t, relation)
+
+	return relation.Id
+}
+
+func TestCreateRelation(t *testing.T) {
+	createRelation(t)
+}
