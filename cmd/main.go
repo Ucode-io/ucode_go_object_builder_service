da96e2588ae1757a5eeec5a7ee283e5b3618cc7c
Shokhrukh <shokhrukhsafarov2003@gmail.com>
2024-04-24 09:02:53 +0500

    removed log from main.go

--- cmd/main.go
+++ cmd/main.go
@@ -2,12 +2,10 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"net"
 	"ucode/ucode_go_object_builder_service/config"
 	"ucode/ucode_go_object_builder_service/grpc"
 	"ucode/ucode_go_object_builder_service/grpc/client"
-	"ucode/ucode_go_object_builder_service/models"
 	"ucode/ucode_go_object_builder_service/pkg/logger"
 	"ucode/ucode_go_object_builder_service/storage/postgres"
 
@@ -40,18 +38,6 @@ func main() {
 		log.Panic("postgres.NewPostgres", logger.Error(err))
 	}
 	defer pgStore.CloseDB()
-
-	resp, err := pgStore.Relation().GetSingleViewForRelation(context.Background(), models.ReqForViewRelation{
-		Id:        "426a0cd6-958d-4317-bf23-3b4ea4720e53",
-		TableSlug: "fuck",
-		RoleId:    "4072dc4a-d0ef-476c-bb59-6522230f3210",
-	})
-	if err != nil {
-		fmt.Println("Err->", err)
-		return
-	}
-	fmt.Println("Resp=>", resp)
-	return
 
 	svcs, err := client.NewGrpcClients(cfg)
 	if err != nil {
