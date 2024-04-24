2cc239c1ce4186c0e293778e1fdcd121d91e023b
o.muzropov <otabek.muzropov2001@gmail.com>
2024-04-24 10:08:36 +0500

    relations getlist and single are fixed

--- pkg/helper/relation.go
+++ pkg/helper/relation.go
@@ -484,7 +484,7 @@ func ExecRelation(ctx context.Context, req RelationHelper) error {
 	addConstraintSQL := fmt.Sprintf(`
         ALTER TABLE IF EXISTS %s
         ADD CONSTRAINT fk_%s_%s_id
-        FOREIGN KEY (%s_id) REFERENCES %s(id);
+        FOREIGN KEY (%s_id) REFERENCES %s(guid);
     `, req.TableTo, req.TableTo, req.TableFrom, req.TableFrom, req.TableFrom)
 
 	_, err := req.Tx.Exec(ctx, alterTableSQL)
