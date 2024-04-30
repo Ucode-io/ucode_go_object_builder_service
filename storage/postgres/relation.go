0394fb8b3858f74a8287b402cb4409e3c92870bb
b3atez <azamatn676@gmail.com>
2024-04-30 10:52:52 +0500

    fix relatio many2many create field_permission to table_to

--- storage/postgres/relation.go
+++ storage/postgres/relation.go
@@ -276,7 +276,7 @@ func (r *relationRepo) Create(ctx context.Context, data *nb.CreateRelationReques
 		err = helper.RelationFieldPermission(ctx, helper.RelationHelper{
 			Tx:        tx,
 			FieldID:   field.Id,
-			TableSlug: data.TableFrom,
+			TableSlug: data.TableTo,
 			Label:     "FROM " + data.TableFrom + " TO " + data.TableTo,
 			RoleIDs:   roles,
 		})
