172ad6fa3fb70dc9f9fdc1e05eadc1eb074a2590
b3atez <azamatn676@gmail.com>
2024-06-03 08:41:43 +0500

    fix search table

--- storage/postgres/table.go
+++ storage/postgres/table.go
@@ -354,7 +354,7 @@ func (t *tableRepo) GetAll(ctx context.Context, req *nb.GetAllTablesRequest) (re
 	FROM "table" WHERE 1=1`
 
 	if req.Search != "" {
-		query += ` label ilike %:label% `
+		query += ` label ~* :label `
 		params["label"] = req.Search
 	}
 
