ffc9715d4b83c6cf42e67d0ded68f481caefa88f
b3atez <azamatn676@gmail.com>
2024-06-06 13:41:14 +0500

    fix

--- grpc/service/builder_project.go
+++ grpc/service/builder_project.go
@@ -238,24 +238,22 @@ func (b *builderProjectService) AutoConnect(ctx context.Context) error {
 			continue
 		}
 
-		if resource.Credentials.Username == "uno_1224095bddd1479fb259cc7625fff13d_p_postgres_svcs" {
-
-			_, err = b.Reconnect(ctx, &nb.RegisterProjectRequest{
-				Credentials: &nb.RegisterProjectRequest_Credentials{
-					Host:     resource.GetCredentials().GetHost(),
-					Port:     resource.GetCredentials().GetPort(),
-					Database: resource.GetCredentials().GetDatabase(),
-					Password: resource.GetCredentials().GetPassword(),
-					Username: resource.GetCredentials().GetUsername(),
-				},
-				ProjectId: resource.GetProjectId(),
-				// K8SNamespace: resource.,
-			})
-			if err != nil {
-				b.log.Error("!!!AutoConnect-->Reconnect", logger.Error(err))
-				return err
-			}
+		_, err = b.Reconnect(ctx, &nb.RegisterProjectRequest{
+			Credentials: &nb.RegisterProjectRequest_Credentials{
+				Host:     resource.GetCredentials().GetHost(),
+				Port:     resource.GetCredentials().GetPort(),
+				Database: resource.GetCredentials().GetDatabase(),
+				Password: resource.GetCredentials().GetPassword(),
+				Username: resource.GetCredentials().GetUsername(),
+			},
+			ProjectId: resource.GetProjectId(),
+			// K8SNamespace: resource.,
+		})
+		if err != nil {
+			b.log.Error("!!!AutoConnect-->Reconnect", logger.Error(err))
+			return err
 		}
+
 	}
 
 	return nil
