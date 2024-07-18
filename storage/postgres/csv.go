ff1c608de71d938a901e674d00067582a7cf22a5
Shokhrukh Safarov <safarovshohruh2803@gmail.com>
2024-07-18 15:43:24 +0500

    csv

--- storage/postgres/csv.go
+++ storage/postgres/csv.go
@@ -2,8 +2,14 @@ package postgres
 
 import (
 	"context"
+	"encoding/csv"
 	"encoding/json"
 	"fmt"
+	"os"
+	"regexp"
+	"strings"
+	"time"
+	"ucode/ucode_go_object_builder_service/config"
 	nb "ucode/ucode_go_object_builder_service/genproto/new_object_builder_service"
 	"ucode/ucode_go_object_builder_service/models"
 	"ucode/ucode_go_object_builder_service/pkg/helper"
@@ -12,7 +18,11 @@ import (
 
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/lib/pq"
+	"github.com/minio/minio-go/v7"
+	"github.com/minio/minio-go/v7/pkg/credentials"
 	"github.com/spf13/cast"
+	"google.golang.org/protobuf/encoding/protojson"
+	"google.golang.org/protobuf/types/known/structpb"
 )
 
 type csvRepo struct {
@@ -25,7 +35,8 @@ func NewCSVRepo(db *pgxpool.Pool) storage.CSVRepoI {
 	}
 }
 
-func (c *csvRepo) GetListInCSV(ctx context.Context, req *nb.CommonMessage) (resp *nb.CommonMessage, err error) {
+func (o *csvRepo) GetListInCSV(ctx context.Context, req *nb.CommonMessage) (resp *nb.CommonMessage, err error) {
+
 	conn := psqlpool.Get(req.GetProjectId())
 
 	var (
@@ -42,8 +53,11 @@ func (c *csvRepo) GetListInCSV(ctx context.Context, req *nb.CommonMessage) (resp
 
 	fieldIds := cast.ToStringSlice(params["field_ids"])
 
-	getFieldsQuery := `SELECT f.type, f.slug, f.attributes, f.label FROM "field" f WHERE f.id = ANY ($1)`
-	fieldRows, err := conn.Query(ctx, getFieldsQuery, pq.Array(fieldIds))
+	delete(params, "field_ids")
+
+	query := `SELECT f.type, f.slug, f.attributes, f.label FROM "field" f WHERE f.id = ANY ($1)`
+
+	fieldRows, err := conn.Query(ctx, query, pq.Array(fieldIds))
 	if err != nil {
 		return &nb.CommonMessage{}, err
 	}
@@ -85,8 +99,120 @@ func (c *csvRepo) GetListInCSV(ctx context.Context, req *nb.CommonMessage) (resp
 		return &nb.CommonMessage{}, err
 	}
 
-	fmt.Println("Fields-->", fieldsArr)
-	fmt.Println("Items--->", items)
+	filename := fmt.Sprintf("report_%d.csv", time.Now().Unix())
+	filepath := "./" + filename
+	file, err := os.Create(filepath)
+	if err != nil {
+		return &nb.CommonMessage{}, err
+	}
+	defer file.Close()
 
-	return nil, nil
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+
+	headers := []string{}
+	for _, field := range fieldsArr {
+		headers = append(headers, field.Label)
+	}
+	if err := writer.Write(headers); err != nil {
+		return &nb.CommonMessage{}, err
+	}
+
+	for _, item := range items {
+		record := []string{}
+		for _, field := range fieldsArr {
+			if field.Type == "MULTI_LINE" {
+				re := regexp.MustCompile(`<[^>]+>`)
+				result := re.ReplaceAllString(cast.ToString(item[field.Slug]), "")
+				item[field.Slug] = result
+			} else if field.Type == "DATE" {
+				timeF, err := time.Parse("2006-01-02", strings.Split(cast.ToString(item[field.Slug]), " ")[0])
+				if err != nil {
+					return &nb.CommonMessage{}, err
+				}
+				item[field.Slug] = timeF.Format("02.01.2006")
+			} else if field.Type == "DATE_TIME" {
+				newTime := strings.Split(cast.ToString(item[field.Slug]), " ")[0] + " " + strings.Split(cast.ToString(item[field.Slug]), " ")[1]
+				timeF, err := time.Parse("2006-01-02 15:04:05", newTime)
+				if err != nil {
+					return &nb.CommonMessage{}, err
+				}
+				item[field.Slug] = timeF.Format("02.01.2006 15:04")
+			} else if field.Type == "MULTISELECT" {
+				attributes, err := helper.ConvertStructToMap(field.Attributes)
+				if err != nil {
+					return &nb.CommonMessage{}, err
+				}
+				multiselectValue := ""
+				if options, ok := attributes["options"].([]interface{}); ok {
+					values := cast.ToStringSlice(item[field.Slug])
+					for _, val := range values {
+						for _, op := range options {
+							opt := cast.ToStringMap(op)
+							if val == cast.ToString(opt["value"]) {
+								if label, ok := opt["label"].(string); ok && label != "" {
+									multiselectValue += label + ","
+								} else {
+									multiselectValue += cast.ToString(opt["value"]) + ","
+								}
+							}
+						}
+					}
+				}
+				item[field.Slug] = strings.TrimRight(multiselectValue, ",")
+			}
+			record = append(record, cast.ToString(item[field.Slug]))
+		}
+		if err := writer.Write(record); err != nil {
+			return &nb.CommonMessage{}, err
+		}
+	}
+
+	cfg := config.Load()
+
+	endpoint := cfg.MinioHost
+	accessKeyID := cfg.MinioAccessKeyID
+	secretAccessKey := cfg.MinioSecretKey
+
+	minioClient, err := minio.New(endpoint, &minio.Options{
+		Creds:  credentials.NewStaticV4(accessKeyID, secretAccessKey, ""),
+		Secure: true,
+	})
+	if err != nil {
+		return &nb.CommonMessage{}, err
+	}
+
+	_, err = minioClient.FPutObject(
+		context.Background(),
+		"reports",
+		filename,
+		filepath,
+		minio.PutObjectOptions{ContentType: "text/csv"},
+	)
+	if err != nil {
+		return &nb.CommonMessage{}, err
+	}
+
+	err = os.Remove(filepath)
+	if err != nil {
+		return &nb.CommonMessage{}, err
+	}
+
+	link := fmt.Sprintf("%s/reports/%s", endpoint, filename)
+	respCSV := map[string]string{
+		"link": link,
+	}
+
+	marshaledInputMap, err := json.Marshal(respCSV)
+	outputStruct := &structpb.Struct{}
+	if err != nil {
+		return &nb.CommonMessage{}, err
+	}
+
+	err = protojson.Unmarshal(marshaledInputMap, outputStruct)
+	if err != nil {
+		return &nb.CommonMessage{}, err
+	}
+
+	return &nb.CommonMessage{TableSlug: req.TableSlug, Data: outputStruct}, nil
 }
