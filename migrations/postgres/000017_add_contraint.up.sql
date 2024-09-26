0fdb2eaa5fc6dec2eb3b8d8ef23051d459fb3ee7
OtabekMuzropov <otabek.muzropov2001@gmail.com>
2024-09-26 10:51:06 +0500

    login table

--- migrations/postgres/000017_add_contraint.up.sql
+++ migrations/postgres/000017_add_contraint.up.sql
@@ -1,1 +1,1 @@
-ALTER TABLE IF EXISTS "field" DROP CONSTRAINT IF EXISTS "field_table_id_slug_unique";
+ALTER TABLE IF EXISTS "field" ADD CONSTRAINT "field_table_id_slug_unique" UNIQUE (table_id, slug);
