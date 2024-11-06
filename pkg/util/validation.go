ae0efcadb478bafeff9ae627f71464088b8040db
OtabekMuzropov <otabek.muzropov2001@gmail.com>
2024-11-06 15:23:12 +0500

    add strong password checker logic for password field type

---
+++ pkg/util/validation.go
@@ -0,0 +1,27 @@
+package util
+
+import (
+	"errors"
+	"regexp"
+)
+
+func ValidStrongPassword(password string) error {
+	if len(password) < 6 {
+		return errors.New("password must be at least 8 characters long")
+	}
+
+	if !regexp.MustCompile(`[A-Z]`).MatchString(password) {
+		return errors.New("password must contain at least one uppercase letter")
+	}
+	if !regexp.MustCompile(`[a-z]`).MatchString(password) {
+		return errors.New("password must contain at least one lowercase letter")
+	}
+	if !regexp.MustCompile(`\d`).MatchString(password) {
+		return errors.New("password must contain at least one digit")
+	}
+	if !regexp.MustCompile(`[!@#~$%^&*(),.?":{}|<>]`).MatchString(password) {
+		return errors.New("password must contain at least one special character")
+	}
+
+	return nil
+}
