af3b31dbc53c41e12bbdbf17450e901a83d3fc56
Abdulbosit <abdulbositkabilov1@gmail.com>
2025-01-08 14:44:07 +0500

    update 21 migration

--- migrations/postgres/000021_create_table.down.sql
+++ migrations/postgres/000021_create_table.down.sql
@@ -3,3 +3,7 @@ DROP TABLE IF EXISTS "person";
 DELETE FROM "table" WHERE id = 'c1669d87-332c-41ee-84ac-9fb2ac9efdd5';
 
 DELETE FROM "field" WHERE table_id = 'c1669d87-332c-41ee-84ac-9fb2ac9efdd5';
+
+DELETE FROM "record_permission" WHERE table_slug = 'person';
+
+DELETE FROM "field_permission" WHERE table_slug = 'person';
