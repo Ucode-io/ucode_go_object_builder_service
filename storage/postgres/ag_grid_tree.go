31e972012ce9e3dd0e4df888d5ddf7a28616f00f
Shokhrukh Safarov <safarovshohruh2803@gmail.com>
2025-01-31 09:39:06 +0500

    changed response code in ag grid tree

--- storage/postgres/ag_grid_tree.go
+++ storage/postgres/ag_grid_tree.go
@@ -6,13 +6,13 @@ import (
 	"fmt"
 	"strings"
 
+	"ucode/ucode_go_object_builder_service/config"
 	nb "ucode/ucode_go_object_builder_service/genproto/new_object_builder_service"
 	"ucode/ucode_go_object_builder_service/pkg/helper"
 	psqlpool "ucode/ucode_go_object_builder_service/pool"
 
 	"github.com/pkg/errors"
 	"github.com/spf13/cast"
-	"google.golang.org/protobuf/types/known/structpb"
 )
 
 func (o *objectBuilderRepo) AgGridTree(ctx context.Context, req *nb.CommonMessage) (resp *nb.CommonMessage, err error) {
@@ -88,11 +88,11 @@ func (o *objectBuilderRepo) AgGridTree(ctx context.Context, req *nb.CommonMessag
 			val := values[i]
 			if val == nil {
 				rowMap[colName] = nil
-			} else if colName == "guid" || strings.Contains(colName, "_id") {
+			} else if colName == config.GUID || strings.Contains(colName, config.ID) {
 				if arr, ok := val.([16]byte); ok {
 					rowMap[colName] = helper.ConvertGuid(arr)
 				}
-			} else if colName == "path" {
+			} else if colName == config.PATH {
 				if arr, ok := val.([]interface{}); ok {
 					var guidList []string
 					for _, guid := range arr {
@@ -113,23 +113,18 @@ func (o *objectBuilderRepo) AgGridTree(ctx context.Context, req *nb.CommonMessag
 		return nil, errors.Wrap(err, "error while iterating over rows")
 	}
 
-	jsonBytes, err := json.Marshal(results)
-	if err != nil {
-		return &nb.CommonMessage{}, errors.Wrap(err, "error while marshalling client types")
+	response := map[string]interface{}{
+		"response": results,
 	}
-
-	var dataStruct structpb.Struct
-	jsonBytes = []byte(fmt.Sprintf(`{"response": %s}`, jsonBytes))
-
-	err = json.Unmarshal(jsonBytes, &dataStruct)
+	newResp, err := helper.ConvertMapToStruct(response)
 	if err != nil {
-		return &nb.CommonMessage{}, errors.Wrap(err, "error while unmarshalling client types")
+		return nil, errors.Wrap(err, "error while converting map to struct")
 	}
 
 	return &nb.CommonMessage{
 		TableSlug: req.TableSlug,
 		ProjectId: req.ProjectId,
-		Data:      &dataStruct,
+		Data:      newResp,
 	}, nil
 }
 
