094d45538f90623c1084d42d0fcb4e75736f47a7
webslingerr <shokhrukhsafarov2003@gmail.com>
2025-04-21 14:17:38 +0500

    field type in chart

--- storage/postgres/table.go
+++ storage/postgres/table.go
@@ -1146,6 +1146,7 @@ func (t *tableRepo) GetChart(ctx context.Context, req *nb.ChartPrimaryKey) (resp
 	conn := psqlpool.Get(req.GetProjectId())
 
 	tables := map[string]*nb.Table{}
+	tableSlugs := map[string]bool{}
 	rows, err := conn.Query(ctx, `
         SELECT id, label, slug 
         FROM public.table 
@@ -1163,6 +1164,7 @@ func (t *tableRepo) GetChart(ctx context.Context, req *nb.ChartPrimaryKey) (resp
 			return &nb.GetChartResponse{}, err
 		}
 		tables[table.Id] = table
+		tableSlugs[table.Label] = true
 	}
 
 	fields := map[string][]*nb.Field{}
@@ -1181,7 +1183,7 @@ func (t *tableRepo) GetChart(ctx context.Context, req *nb.ChartPrimaryKey) (resp
 		if err = rows.Scan(&tableID, &slug, &fieldType); err != nil {
 			return &nb.GetChartResponse{}, err
 		}
-		fields[tableID] = append(fields[tableID], &nb.Field{Slug: slug, Type: fieldType})
+		fields[tableID] = append(fields[tableID], &nb.Field{Slug: slug, Type: helper.FIELD_TYPES[fieldType]})
 	}
 
 	relations := []*models.RelationForView{}
@@ -1200,12 +1202,14 @@ func (t *tableRepo) GetChart(ctx context.Context, req *nb.ChartPrimaryKey) (resp
 		if err = rows.Scan(&tableFrom, &tableTo, &fieldFrom, &fieldTo); err != nil {
 			return &nb.GetChartResponse{}, err
 		}
-		relations = append(relations, &models.RelationForView{
-			TableFrom: tableFrom,
-			TableTo:   tableTo,
-			FieldFrom: fieldFrom,
-			FieldTo:   fieldTo,
-		})
+		if tableSlugs[tableFrom] && tableSlugs[tableTo] {
+			relations = append(relations, &models.RelationForView{
+				TableFrom: tableFrom,
+				TableTo:   tableTo,
+				FieldFrom: fieldFrom,
+				FieldTo:   fieldTo,
+			})
+		}
 	}
 
 	type tableOutput struct {
