d258d2668fa531ccd8563a9dc2453ed5170f7c34
OtabekMuzropov <otabek.muzropov2001@gmail.com>
2025-05-06 08:23:36 +0500

    add login register type for /register api

--- grpc/service/builder_project.go
+++ grpc/service/builder_project.go
@@ -235,9 +235,9 @@ func (b *builderProjectService) AutoConnect(ctx context.Context) error {
 			continue
 		}
 
-		// if resource.GetCredentials().GetDatabase() != "hasura_6fd296f691954ed3af84c1dcca929273_p_postgres_svcs" {
-		// 	continue
-		// }
+		if resource.GetCredentials().GetDatabase() != "humanizati_27ab570d10874ad8b1a44a0425092a0f_p_postgres_svcs" {
+			continue
+		}
 
 		b.log.Info(
 			fmt.Sprintf(
