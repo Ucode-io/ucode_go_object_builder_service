0937729ba6e4192dc99744a2e3366fecf42ae8b6
webslingerr <shokhrukhsafarov2003@gmail.com>
2025-05-29 10:19:57 +0500

    date time panic changed in get list in excel

--- storage/postgres/object_builder.go
+++ storage/postgres/object_builder.go
@@ -1697,12 +1697,12 @@ func (o *objectBuilderRepo) GetListInExcel(ctx context.Context, req *nb.CommonMe
 
 					item[f.Slug] = timeF.Format("02.01.2006")
 				} else if f.Type == "DATE_TIME" {
-					t, err := time.Parse("2006-01-02 15:04:05 -0700 MST", cast.ToString(item[f.Slug]))
+					t, err := time.Parse(config.DateTimeWithZone, cast.ToString(item[f.Slug]))
 					if err != nil {
 						item[f.Slug] = ""
-						continue
+					} else {
+						item[f.Slug] = t.Format(time.DateTime)
 					}
-					item[f.Slug] = t.Format(time.DateTime)
 				} else if f.Type == "MULTISELECT" {
 					attributes, err := helper.ConvertStructToMap(f.Attributes)
 					if err != nil {
