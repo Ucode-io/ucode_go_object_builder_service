9d92f3de71fd0356e5cb5da2bcabc554a23c6f18
webslingerr <shokhrukhsafarov2003@gmail.com>
2025-06-26 11:01:34 +0500

    where search in board

--- storage/postgres/object_builder.go
+++ storage/postgres/object_builder.go
@@ -2774,21 +2774,45 @@ func (o *objectBuilderRepo) GetBoardData(ctx context.Context, req *nb.CommonMess
 		))
 	}
 
+	var (
+		whereBuilder strings.Builder
+		queryParams  = []any{offset, limit}
+		paramIdx     = 3
+	)
+	whereBuilder.WriteString("a.deleted_at IS NULL")
+
+	skipFields := map[string]bool{
+		"fields":      true,
+		"view_fields": true,
+	}
+	for key, value := range req.Data.AsMap() {
+		if _, ok := value.([]any); !ok || skipFields[key] {
+			continue
+		}
+		whereBuilder.WriteString(fmt.Sprintf(" AND a.%s = ANY($%d)",
+			pq.QuoteIdentifier(key),
+			paramIdx,
+		))
+		queryParams = append(queryParams, value)
+		paramIdx++
+	}
+
 	query := fmt.Sprintf(`
 			SELECT
 				%s
 			FROM %s a
-			WHERE a.deleted_at IS NULL
+			WHERE %s
 			ORDER BY a.%s ASC, a.board_order ASC, a.updated_at DESC
 			OFFSET $1
 			LIMIT $2
 		`,
 		sb.String(),
 		pq.QuoteIdentifier(req.TableSlug),
+		whereBuilder.String(),
 		pq.QuoteIdentifier(orderBy),
 	)
 
-	rows, err := conn.Query(ctx, query, offset, limit)
+	rows, err := conn.Query(ctx, query, queryParams...)
 	if err != nil {
 		return nil, helper.HandleDatabaseError(err, o.logger, "GetBoardData: Failed to query db")
 	}
