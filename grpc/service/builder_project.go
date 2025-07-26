935846a439b05419a04a3c00213089a1871684fc
webslingerr <shokhrukhsafarov2003@gmail.com>
2025-07-26 15:48:38 +0500

    removed if skip

--- grpc/service/builder_project.go
+++ grpc/service/builder_project.go
@@ -235,9 +235,9 @@ func (b *builderProjectService) AutoConnect(ctx context.Context) error {
 			continue
 		}
 
-		if resource.GetCredentials().GetDatabase() != "dbml_a504f6ccce284971aaf20d044a30351b_p_postgres_svcs" {
-			continue
-		}
+		// if resource.GetCredentials().GetDatabase() != "dbml_a504f6ccce284971aaf20d044a30351b_p_postgres_svcs" {
+		// 	continue
+		// }
 
 		b.log.Info(
 			fmt.Sprintf(
