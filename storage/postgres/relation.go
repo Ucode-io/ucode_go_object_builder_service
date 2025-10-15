c7bd51a65771a3ed555351ad070677ac2bf96b27
OtabekMuzropov <otabek.muzropov2001@gmail.com>
2025-10-15 11:10:11 +0500

    fix query syntax problem with boolean column

--- storage/postgres/relation.go
+++ storage/postgres/relation.go
@@ -2098,8 +2098,11 @@ func (r *relationRepo) Update(ctx context.Context, data *nb.UpdateRelationReques
 		return nil, errors.Wrap(err, "failed to marshal")
 	}
 
-	updateField := fmt.Sprintf("UPDATE field SET attributes='%v', required='%v' WHERE relation_id='%v'", string(jsonAttr), data.Required, data.Id)
-	_, err = tx.Exec(ctx, updateField)
+	updateField := `UPDATE "field" SET 
+		attributes=$2, 
+		required=$3 
+	WHERE relation_id=$1`
+	_, err = tx.Exec(ctx, updateField, data.Id, string(jsonAttr), data.Required)
 	if err != nil {
 		return nil, errors.Wrap(err, "cannot update field")
 	}
