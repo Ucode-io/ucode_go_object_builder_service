a21bb77d78901fc6222390ccb85f9b7ee763e8d2
mac <hayrullaev008@gmail.com>
2026-05-12 17:10:59 +0500

    mcp_project added a collumn

--- storage/postgres/mcp_projects.go
+++ storage/postgres/mcp_projects.go
@@ -54,8 +54,8 @@ func (m *mcpProjectRepo) CreateMcpProject(ctx context.Context, req *nb.CreateMcp
 		now       = time.Now()
 
 		projectQuery = `
-			INSERT INTO mcp_project (id, title, description, project_env, ucode_project_id, api_key, environment_id, status, app_visibility, created_at, updated_at)
-			VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $10)`
+			INSERT INTO mcp_project (id, title, description, project_env, ucode_project_id, api_key, environment_id, status, app_visibility, is_published, created_at, updated_at)
+			VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, false, $10, $10)`
 	)
 
 	_, err = tx.Exec(ctx, projectQuery, projectId, req.GetTitle(), req.GetDescription(), req.ProjectEnv.AsMap(),
@@ -217,6 +217,10 @@ func (m *mcpProjectRepo) updateProjectFields(ctx context.Context, tx pgx.Tx, req
 		argIndex++
 	}
 
+	setClauses = append(setClauses, fmt.Sprintf("is_published = $%d", argIndex))
+	args = append(args, req.GetIsPublished())
+	argIndex++
+
 	args = append(args, req.GetId())
 
 	var query = fmt.Sprintf(`
@@ -310,12 +314,12 @@ func (m *mcpProjectRepo) GetAllMcpProject(ctx context.Context, req *nb.GetMcpPro
 	)
 
 	queryBuilder.WriteString(`
-        SELECT mp.id, mp.title, mp.description, mp.project_env, mp.ucode_project_id, mp.api_key, mp.environment_id, mp.status, mp.app_visibility, mp.created_at, mp.updated_at,
+        SELECT mp.id, mp.title, mp.description, mp.project_env, mp.ucode_project_id, mp.api_key, mp.environment_id, mp.status, mp.app_visibility, mp.is_published, mp.created_at, mp.updated_at,
                f.id, f.name, f.path, f.type, f.url, f.branch, f.repo_id,
                f.created_at, f.updated_at
         FROM mcp_project AS mp
         LEFT JOIN function AS f ON mp.function_id = f.id
-        WHERE 1=1
+        WHERE mp.is_published = true
     `)
 
 	if len(req.GetIds()) > 0 {
@@ -365,7 +369,7 @@ func (m *mcpProjectRepo) GetAllMcpProject(ctx context.Context, req *nb.GetMcpPro
 		project.FunctionData = &nb.FunctionData{}
 
 		err = rows.Scan(
-			&project.Id, &project.Title, &project.Description, &projectEnv, &ucodeProjectId, &apiKey, &environmentId, &status, &appVisibility, &createdAt, &updatedAt,
+			&project.Id, &project.Title, &project.Description, &projectEnv, &ucodeProjectId, &apiKey, &environmentId, &status, &appVisibility, &project.IsPublished, &createdAt, &updatedAt,
 			&fId, &fName, &fPath, &fType, &fUrl, &fBranch, &fRepoId, &fCreatedAt, &fUpdatedAt,
 		)
 		if err != nil {
@@ -443,8 +447,8 @@ func (m *mcpProjectRepo) GetMcpProjectFiles(ctx context.Context, req *nb.McpProj
 		fCreatedAt, fUpdatedAt                           sql.NullTime
 
 		projectQuery = `
-        	SELECT 
-            	mp.id, mp.title, mp.description, mp.project_env, mp.ucode_project_id, mp.api_key, mp.environment_id, mp.status, mp.app_visibility, mp.created_at, mp.updated_at,
+        	SELECT
+            	mp.id, mp.title, mp.description, mp.project_env, mp.ucode_project_id, mp.api_key, mp.environment_id, mp.status, mp.app_visibility, mp.is_published, mp.created_at, mp.updated_at,
             	f.id, f.name, f.path, f.type, f.url, f.branch, f.repo_id, f.created_at, f.updated_at
         	FROM mcp_project mp
         	LEFT JOIN function f ON mp.function_id = f.id
@@ -455,7 +459,7 @@ func (m *mcpProjectRepo) GetMcpProjectFiles(ctx context.Context, req *nb.McpProj
 	var ucodeProjectId, apiKey, environmentId, pStatus, appVisibility sql.NullString
 
 	err = conn.QueryRow(ctx, projectQuery, req.GetId()).Scan(
-		&project.Id, &project.Title, &project.Description, &projectEnv, &ucodeProjectId, &apiKey, &environmentId, &pStatus, &appVisibility, &pCreatedAt, &pUpdatedAt,
+		&project.Id, &project.Title, &project.Description, &projectEnv, &ucodeProjectId, &apiKey, &environmentId, &pStatus, &appVisibility, &project.IsPublished, &pCreatedAt, &pUpdatedAt,
 		&fId, &fName, &fPath, &fType, &fUrl, &fBranch, &fRepoId, &fCreatedAt, &fUpdatedAt,
 	)
 	if err != nil {
