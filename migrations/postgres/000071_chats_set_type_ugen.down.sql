6ccec75e8bf481e260637ddb624a0b6bc5e62d49
mac <hayrullaev008@gmail.com>
2026-06-25 08:11:18 +0500

    migrations edited

--- migrations/postgres/000071_chats_set_type_ugen.down.sql
+++ migrations/postgres/000071_chats_set_type_ugen.down.sql
@@ -1,2 +1,7 @@
-UPDATE chats
-SET type = 'ucode';
+DO $$
+BEGIN
+    IF to_regclass('public.chats') IS NOT NULL THEN
+        UPDATE chats SET type = 'ucode';
+    END IF;
+END
+$$;
--- migrations/postgres/000071_chats_set_type_ugen.up.sql
+++ migrations/postgres/000071_chats_set_type_ugen.up.sql
@@ -1,2 +1,7 @@
-UPDATE chats
-SET type = 'ugen';
+DO $$
+BEGIN
+    IF to_regclass('public.chats') IS NOT NULL THEN
+        UPDATE chats SET type = 'ugen';
+    END IF;
+END
+$$;
